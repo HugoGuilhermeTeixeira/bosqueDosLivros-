aa12361416e290becd47b1fcd46cb58a0e59cbc5
Gabryelly15 <144458759+Gabryelly15@users.noreply.github.com>
2023-11-26 20:18:54 -0300

    Update usuario.php

--- front/usuario.php
+++ front/usuario.php
@@ -1,8 +1,5 @@
-<?php
-include('protect.php');
-?>
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <script src="https://kit.fontawesome.com/ea97a3051d.js"
     crossorigin="anonymous"></script>
@@ -10,6 +7,7 @@ include('protect.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="usuario.css">
     <link rel="stylesheet" type="text/css" href="carousel.css"> <!-- Adicionando o CSS para o carrossel -->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <title>usuario</title>
 </head>
 
@@ -20,15 +18,16 @@ include('protect.php');
           <h3 class="titulo">BOSQUE DOS LIVROS</h3>
         </span>
         <div class="navigations">
-        <a id="focused" class="header__link" href="index.html">
+          <a id="focused" class="header__link" href="#about">
             <i class="fa-solid fa-house"></i>HOME
           </a>
           <a class="header__link" href="#experience">
             <i class="fa-solid fa-address-card"></i>SOBRE NÓS</a
           >
-          <a class="header__link" href="../index.html#contatos">
-            <i class="fa-solid fa-envelope"></i>CONTATOS</a
-          >
+          <a class="header__link" href="#projects">
+            <i class="fa-solid fa-envelope"></i>CONTATOS</a>
+        </div>
+        <a class="header_link_sign" type="submit" href="#signup"> SIGN OUT </a>
       </header>
 
     <div class="user-page">
@@ -44,28 +43,69 @@ include('protect.php');
 
     <div class="add-book-form">
         <h2>SEJA UM DOADOR!<br> Cadastre um Livro:</h2>
+        <br>
         <form>
             <label for="bookTitle">Titulo:</label>
-            <input type="text" id="bookTitle" placeholder="Enter the Titulo" required>
+            <input type="text" id="bookTitle" placeholder="Digite o Titulo" required>
 
             <label for="author">Autor:</label>
-            <input type="text" id="author" placeholder="Enter the Autor" required>
+            <input type="text" id="author" placeholder="Digite o Autor" required>
 
             <label for="author">Editora:</label>
-            <input type="text" id="Editora" placeholder="Enter the Editora" required>
+            <input type="text" id="Editora" placeholder="Digite a Editora" required>
 
             <label for="author">Gênero:</label>
-            <input type="text" id="Gênero" placeholder="Enter the Gênero" required>
+            <input type="text" id="Gênero" placeholder="Digite o Gênero" required>
 
             <label for="author">Sinopse:</label>
-            <input type="text" id="Sinopse" placeholder="Enter the Sinopse" required>
+            <input type="text" id="Sinopse" placeholder="Digite a Sinopse" required><br>
 
             <button type="button">CADASTRAR</button>
         </form>
     </div>
 
-</body>
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script>
+        // Dados de exemplo (substitua pelos seus dados reais)
+        var generos = ["Ficção", "Romance", "Mistério", "Fantasia"];
+        var quantidades = [30, 20, 15, 35];
+    
+        var ctx = document.getElementById('pieChart').getContext('2d');
+        var pieChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: generos,
+                datasets: [{
+                    data: quantidades,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.7)',
+                        'rgba(54, 162, 235, 0.7)',
+                        'rgba(255, 206, 86, 0.7)',
+                        'rgba(75, 192, 192, 0.7)',
+                        // Adicione mais cores conforme necessário
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        // Adicione mais cores conforme necessário
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                maintainAspectRatio: false,
+                legend: {
+                    position: 'bottom',
+                }
+            }
+        });
+    </script>
 
+</body>
+<canvas id="pieChart" width="400" height="400"></canvas>
 <footer>
     <span>
         <h1>Contate-nos</h1>
