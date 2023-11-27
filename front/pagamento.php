05194857a15ff0c1dbd50c6f2e25bf0c95db4919
Hugo Teixeira <hugogteixeira2004@gmail.com>
2023-11-26 21:59:59 -0300

    new file:   back/api/Correios.Demo.csproj 	new file:   back/api/Correios.Demo.sln 	new file:   back/api/CorreiosManager.cs 	new file:   back/api/Item.cs 	new file:   back/api/Program.cs 	deleted:    back/cadastro.php 	modified:   front/pagamento.php  Untracked files: 	back/cadastrodb.php

--- front/pagamento.php
+++ front/pagamento.php
@@ -7,7 +7,7 @@
     ></script>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="teste.css" />
+    <link rel="stylesheet" href="stylePagamentos.css" />
     <title>Página de Pagamento</title>
   </head>
   <body>
