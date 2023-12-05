e542d27e391c9c3d1df09771dc0a7af310431c41
Gabryelly15 <144458759+Gabryelly15@users.noreply.github.com>
2023-12-05 16:53:25 -0300

    Update login.php

--- front/login.php
+++ front/login.php
@@ -10,7 +10,7 @@ include('../back/exceptions.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="styleLogin.css">
 </head>
 <body>
     <header class="header">
